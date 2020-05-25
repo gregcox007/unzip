f7ce0c23ef0c64b3bcd52b2b6c8bf93240e9800c
gregcox007 <63885007+gregcox007@users.noreply.github.com>
2020-05-25 16:14:03 -0600

    Update unzip_remove.sh

--- unzip_remove.sh
+++ unzip_remove.sh
@@ -3,7 +3,7 @@
 # Step 1 - List the Zip Files
 ls *.zip > files.txt
 
-# Step 2 > Unzip the Files
+# Step 2 > Unzip the Files and Removes the Original Zip File
 for zipfile in $(cat "files.txt")
 do
   unzip "$zipfile"
